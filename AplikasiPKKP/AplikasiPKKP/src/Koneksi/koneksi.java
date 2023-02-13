80eea5f78f7b18d84a15a4b9d60556210a8393bd
Zhafirio <88639616+Zhafirio@users.noreply.github.com>
2023-02-13 12:19:11 +0700

    Add files via upload

---
+++ AplikasiPKKP/AplikasiPKKP/src/Koneksi/koneksi.java
@@ -0,0 +1,30 @@
+package Koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+    public static Connection getConnection() {
+        Connection conn = null;
+        String url = "jdbc:mysql://localhost:3306/aplikasi_pkkp";
+        String user = "root";
+        String password = "";
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection c = koneksi.getConnection();
+            System.out.println(String.format("Connected to database %s " + "successfully.", c.getCatalog()));
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    
+}
